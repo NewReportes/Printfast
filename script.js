be715bc2ed18a1ce216f19a270062152e9bfdbc7
c-ojeda <168502992+c-ojeda@users.noreply.github.com>
2024-04-30 21:05:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,22 +8,22 @@
     }
 
     // Obtener los parámetros de la URL
-    var comanda = obtenerParametro('COMANDA');
-    var fecha = obtenerParametro('FECHA');
-    var resumen = obtenerParametro('resumen');
+    var comanda = obtenerParametro('ID Venta');
+    var fecha = obtenerParametro('fecha_hora');
+    var resumen = obtenerParametro('PRINT_PRODUCTOS');
 
     // Convertir la cadena resumen en un array separado por saltos de línea
     var lineasResumen = resumen.split('\n');
 
     // Escribir la información en el documento HTML
     window.onload = function() {
-        document.getElementById('comanda').innerHTML = comanda;
-        document.getElementById('fecha').innerHTML = fecha;
+        document.getElementById('ID Venta').innerHTML = comanda;
+        document.getElementById('fecha_hora').innerHTML = fecha;
         // Mostrar cada línea del resumen en un párrafo separado
         lineasResumen.forEach(function(linea) {
             var parrafo = document.createElement('p');
             parrafo.innerHTML = linea;
-            document.getElementById('resumen').appendChild(parrafo);
+            document.getElementById('PRINT_PRODUCTOS').appendChild(parrafo);
         });
     };
 </script>
